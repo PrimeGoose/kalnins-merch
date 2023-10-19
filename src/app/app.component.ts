5d89808863b4153428e2b7ce2c98cd001a8db8ef
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-10-19 20:46:11 +0100

    update app.component.ts, product-home.component.ts and styles.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,15 @@ import { Component } from "@angular/core";
     <mat-toolbar>
       <div class="flex flex-row w-full justify-evenly">
         <a href="/kalnins-merch">Shop</a>
-        <!-- <a href="/about">about</a> -->
-        <!-- <a href="/partner">partners</a> -->
       </div>
     </mat-toolbar>
+    <!-- <div class="absolute filter blur brightness-50 top-0 left-0 w-[100vw] h-[100vh] -z-0" Style="background-image: url(assets/bg_bajaru2_vakars.jpg);"></div> -->
 
-    <h1 class="flex flex-row w-full justify-center text-4xl font-serif font-black my-20">Kalniņš Merch</h1>
-
-    <div class="text-center flex place-content-center">
-      <router-outlet></router-outlet>
-    </div>
+  <h1 class="flex z-50  text-white relative flex-row w-full justify-center text-4xl font-serif font-black my-20 ">Kalniņš Merch</h1>
+  
+  <div class="relative text-center z-50 flex place-content-center">
+    <router-outlet></router-outlet>
+  </div>
   `,
   styles: [],
 })
--- src/app/products-home/product-home.component.ts
+++ src/app/products-home/product-home.component.ts
@@ -8,16 +8,12 @@ import { Router } from "@angular/router";
     <div class="grid grid-cols-1 640:grid-cols-2 md:grid-cols-3 768:grid-cols-4 gap-4 max-w-screen-1280 ">
       <!-- Loop through products to display them -->
       <div *ngFor="let product of products" class="rounded overflow-hidden shadow-lg">
-        <img class="w-full" [src]="product.imgages[0]" alt="{{ product.name }}" />
+        <img class="w-full" [src]="product.imgages[0]" alt="{{ product.name }}" [routerLink]="['/product', product.id]" />
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{ product.name }}</div>
           <!-- <p class="text-gray-700 text-base">{{ product.price }} {{ product.currency }}</p> -->
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" [routerLink]="['/product', product.id]">
-            View Details
-          </button>
-        </div>
+
       </div>
     </div>
   `,
@@ -25,10 +21,9 @@ import { Router } from "@angular/router";
 })
 export class ProductHomeComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService,private router:Router) {}
+  constructor(private productService: ProductService, private router: Router) {}
   ngOnInit(): void {
     this.products = this.productService.getAllProducts();
-  
   }
   // Get all products
 }

34d1a2843d7ba6e88de95abd2a727542a7f71041
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-10-31 03:36:10 +0000

    update order-success.component.ts

--- src/app/order-success/order-success.component.ts
+++ src/app/order-success/order-success.component.ts
@@ -3,13 +3,13 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-order-success",
   template: `
-    <div class="grid h-screen place-items-center text-center pb-20">
-      <div>
-        <h1 class="text-4xl">PALDIES!</h1>
-        <p>Tavs pieteikums ir pienemts.</p>
-        <p>
-          Ar Tevi pavisam driz sazinasies <strong>MERCH SHOP</strong> apkalposanas specialists lai konkretizetu piegades un apmaksas deta
-          as!
+    <div class="grid h-screen place-items-center content-between text-center">
+      <div class="p-8 bg-white rounded-lg shadow-xl">
+        <h1 class="text-5xl font-bold mb-4 text-pink-600">PALDIES!</h1>
+        <p class="text-xl mb-2">Tavs pieteikums ir pieņemts.</p>
+        <p class="text-lg">
+          Ar Tevi pavisam drīz sazināsies
+          <strong class="text-pink-600">MERCH SHOP</strong> apkalpošanas speciālists, lai konkretizētu piegādes un apmaksas detaļas!
         </p>
       </div>
     </div>

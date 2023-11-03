6c4408b538f6932013ab862bc173a3c9a887f9a3
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-11-03 09:48:08 +0000

    update 10 files

--- convert-png-to-avif.mjs
+++ convert-png-to-avif.mjs
@@ -5,17 +5,17 @@ import sharp from 'sharp';
 const baseDir = './src/assets/';
 
 // Function to delete all .avif files in a directory
-function deleteAVIFFiles(directory) {
+ function deleteAVIFFiles(directory) {
     fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
         if (err) {
             console.error('Error reading the directory for AVIF deletion', err);
             return;
         }
 
-        entries.forEach(entry => {
+        entries.forEach((entry) => {
             const fullPath = path.join(directory, entry.name);
             if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.avif') {
-                fs.unlink(fullPath, err => {
+                 fs.unlink(fullPath, err => {
                     if (err) {
                         console.error(`Error deleting ${fullPath}:`, err);
                     } else {
@@ -28,30 +28,30 @@ function deleteAVIFFiles(directory) {
 }
 
 // Recursive function to process a directory
-function processDirectory(directory) {
+ function processDirectory(directory) {
     // Delete all .avif files before starting conversion
-    deleteAVIFFiles(directory);
+     deleteAVIFFiles(directory);
 
-    fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
+     fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
         if (err) {
             console.error('Error reading the directory for conversion', err);
             return;
         }
 
-        entries.forEach(entry => {
+        entries.forEach( (entry) => {
             const fullPath = path.join(directory, entry.name);
 
             if (entry.isDirectory() && entry.name !== 'icons') {
                 // Recursively process subdirectories, skipping 'icons'
-                processDirectory(fullPath);
+                 processDirectory(fullPath);
             } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.png') {
                 // Convert PNG files to AVIF
                 const outputFile = path.join(directory, path.basename(entry.name, '.png') + '.avif');
 
-                sharp(fullPath)
+                 sharp(fullPath)
                     .avif({
                         quality: 70, // AVIF quality ranges from 0-100
-                        speed: 8, // Speed can range from 0 (slowest) to 8 (fastest)
+                        speed: 0, // Speed can range from 0 (slowest) to 8 (fastest)
                     })
                     .toFile(outputFile)
                     .then(() => {

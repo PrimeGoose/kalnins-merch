42555fe42f150f827d17af810d63926644d37b8b
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-11-03 09:34:33 +0000

    update 30 files and delete 3 files

--- convert-png-to-WebP.mjs
+++ convert-png-to-WebP.mjs
@@ -1,11 +1,26 @@
-// convert-png-to-webp.mjs
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
 const baseDir = './src/assets/';
 
+function deleteWebPFiles(directory) {
+    fs.readdirSync(directory, { withFileTypes: true }).forEach(entry => {
+        const fullPath = path.join(directory, entry.name);
+        if (entry.isDirectory()) {
+            // Recursively delete .webp files in subdirectories
+            deleteWebPFiles(fullPath);
+        } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.webp') {
+            // Delete the .webp file
+            fs.unlinkSync(fullPath);
+            console.log(`Deleted WebP file: ${fullPath}`);
+        }
+    });
+}
+
 function processDirectory(directory) {
+    deleteWebPFiles(directory); // Delete all .webp files before starting the conversion
+
     fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
         if (err) {
             console.error('Error reading the directory', err);
@@ -17,14 +32,14 @@ function processDirectory(directory) {
             if (entry.isDirectory() && entry.name !== 'icons') {
                 // Recursively process subdirectories, skipping 'icons'
                 processDirectory(fullPath);
-            } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.png' && !entry.name.includes('-low.png')) {
+            } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.png') {
                 // Convert PNG files to WebP
                 const outputFile = path.join(directory, path.basename(entry.name, '.png') + '.webp');
 
                 sharp(fullPath)
                     .webp({
-                        quality: 80, // Adjust the quality for a balance of quality and file size
-                        lossless: true, // Set to true for lossless conversion
+                        quality: 80,  // Good for high-quality images without making the file size too large
+                       
                     })
                     .toFile(outputFile)
                     .then(() => {
--- convert-png-to-avif.mjs
+++ convert-png-to-avif.mjs
@@ -1,31 +1,57 @@
-// convert-png-to-avif.mjs
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
 
 const baseDir = './src/assets/';
 
-// Recursive function to process a directory
-function processDirectory(directory) {
+// Function to delete all .avif files in a directory
+function deleteAVIFFiles(directory) {
     fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
         if (err) {
-            console.error('Error reading the directory', err);
+            console.error('Error reading the directory for AVIF deletion', err);
             return;
         }
 
         entries.forEach(entry => {
             const fullPath = path.join(directory, entry.name);
+            if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.avif') {
+                fs.unlink(fullPath, err => {
+                    if (err) {
+                        console.error(`Error deleting ${fullPath}:`, err);
+                    } else {
+                        console.log(`Deleted AVIF file: ${fullPath}`);
+                    }
+                });
+            }
+        });
+    });
+}
+
+// Recursive function to process a directory
+function processDirectory(directory) {
+    // Delete all .avif files before starting conversion
+    deleteAVIFFiles(directory);
+
+    fs.readdir(directory, { withFileTypes: true }, (err, entries) => {
+        if (err) {
+            console.error('Error reading the directory for conversion', err);
+            return;
+        }
+
+        entries.forEach(entry => {
+            const fullPath = path.join(directory, entry.name);
+
             if (entry.isDirectory() && entry.name !== 'icons') {
                 // Recursively process subdirectories, skipping 'icons'
                 processDirectory(fullPath);
-            } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.png' && !entry.name.includes('-low.png')) {
+            } else if (entry.isFile() && path.extname(entry.name).toLowerCase() === '.png') {
                 // Convert PNG files to AVIF
                 const outputFile = path.join(directory, path.basename(entry.name, '.png') + '.avif');
 
                 sharp(fullPath)
                     .avif({
-                        quality: 50, // AVIF quality ranges from 0-100, you might need to experiment with this value
-                        speed: 5, // Speed can range from 0 (slowest) to 8 (fastest). Speed 5 is the default
+                        quality: 70, // AVIF quality ranges from 0-100
+                        speed: 8, // Speed can range from 0 (slowest) to 8 (fastest)
                     })
                     .toFile(outputFile)
                     .then(() => {

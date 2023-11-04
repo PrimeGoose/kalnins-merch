af97e157f43a7c5eccd908746b49742a0bf5805d
Kaspars Bergs <64712362+PrimeGoose@users.noreply.github.com>
2023-11-04 07:47:18 +0000

    update product-image.component.ts and shared.module.ts

--- src/app/shared/components/product-image/product-image.component.ts
+++ src/app/shared/components/product-image/product-image.component.ts
@@ -1,33 +1,24 @@
-import { Component,Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-product-image',
   template: `
-  <picture>
-  <!-- AVIF format -->
-  <source [srcset]="imagePath + '.avif'" type="image/avif" />
-  <!-- WebP format -->
-  <source [srcset]="imagePath + '.webp'" type="image/webp" />
-  <!-- Fallback PNG format -->
-  <img class="w-full" loading="lazy"  [src]="imagePath + '.png'" alt="Product image" />
-</picture>
-
+    <picture>
+      <!-- AVIF format -->
+      <source [srcset]="imagePath + '.avif'" type="image/avif" />
+      <!-- WebP format -->
+      <source [srcset]="imagePath + '.webp'" type="image/webp" />
+      <!-- Fallback PNG format -->
+      <img class="w-full" loading="lazy" [src]="imagePath + '.png'" alt="Product image" />
+    </picture>
   `,
   styles: [
     `
-:host{
-  @apply
-w-full
-pb-[100%]
-
-    
-   
- 
-}
-
-
+      :host {
+        @apply w-full;
+      }
     `,
-  ]
+  ],
 })
 export class ProductImageComponent {
   @Input() imagePath!: string;

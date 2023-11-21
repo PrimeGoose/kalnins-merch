9215f9226bf08ebce7a1b6ff7f045435b9f2a470
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-11-21 17:06:20 +0000

    component for adding product with form validation

--- src/app/features/admin-dashboard-page/admin-dashboard-page.component.ts
+++ src/app/features/admin-dashboard-page/admin-dashboard-page.component.ts
@@ -1,21 +1,20 @@
 import {CommonModule, NgFor} from '@angular/common';
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, FormsModule, NgModel} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, NgModel, ReactiveFormsModule} from '@angular/forms';
 import {Product, Size} from 'src/app/core/services/product.service';
+import {Dimensions, ImageCroppedEvent, ImageCropperComponent, ImageCropperModule, LoadedImage, ImageTransform} from 'ngx-image-cropper';
+import {DomSanitizer} from '@angular/platform-browser';
+import {FormControl, Validators} from '@angular/forms';
+import {AbstractControl, ValidationErrors, FormArray} from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NgFor, FormsModule],
+  imports: [CommonModule, NgFor, FormsModule, ImageCropperModule, ReactiveFormsModule],
   selector: 'app-admin-dashboard-page',
   templateUrl: './admin-dashboard-page.component.html',
   styleUrls: ['./admin-dashboard-page.component.css'],
 })
 export class AdminDashboardPageComponent {
-  selected_size: Size = {
-    size: '',
-    price: 0,
-    available: true,
-  };
   product: Product = {
     category: '',
     id: 0,
@@ -76,10 +75,258 @@ export class AdminDashboardPageComponent {
     images: [],
   };
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private sanitizer: DomSanitizer,
+  ) {}
 
-  selectSize(item: Size) {
-    this.selected_size = item;
+  productForm = this.fb.group({
+    category: ['', Validators.required],
+    name: ['', Validators.required],
+    color: ['', Validators.required],
+    sizes: this.fb.array(
+      this.product.sizes.map((size) => this.fb.group(size)),
+      this.sizeValidator,
+    ),
+    images: [[], this.imageValidator],
+  });
+
+  cropAndAdd() {
+    // Add the cropped image to the product images array
+    this.product.images.push(this.croppedImage);
+
+    // Update the form control with the new images array
+    const imagesControl = this.productForm.get(['images']);
+    if (imagesControl) {
+      const imagesControl = this.productForm.get(['images']) as FormControl<string[]>;
+
+      imagesControl.setValue(this.product.images); // Ensure a new array is set
+      imagesControl.updateValueAndValidity(); // Re-validate the updated form control
+    }
+
+    this.showCropper = false;
+  }
+
+  removeImage(index: number) {
+    // remove image from product.images
+    this.product.images.splice(index, 1);
+
+    // update form control with new images array
+    const imagesControl = this.productForm.get(['images']);
+    if (imagesControl) {
+      const imagesControl = this.productForm.get(['images']) as FormControl<string[]>;
+
+      imagesControl.setValue(this.product.images); // Ensure a new array is set
+      imagesControl.updateValueAndValidity(); // Re-validate the updated form control
+    }
+  }
+
+  sizeValidator(control: AbstractControl): ValidationErrors | null {
+    const sizes = (control as FormArray).controls;
+    const isAvailable = sizes.some((ctrl) => ctrl.value.available);
+    return isAvailable ? null : {sizeRequired: true};
+  }
+
+  get sizesControls() {
+    return (this.productForm.get('sizes') as FormArray).controls;
+  }
+
+  imageValidator(control: AbstractControl): ValidationErrors | null {
+    const images = control.value;
+    const isValid = images && images.length > 0;
+    return isValid ? null : {imageRequired: true};
+  }
+
+  onSubmit() {
+    this.productForm.markAllAsTouched();
+    if (this.productForm.valid) {
+      // Form is valid, proceed with submission logic
+      console.log('Form Value:', this.productForm.value);
+      // console.log('Images Control Value:', this.productForm.get('images')?.value);
+    } else {
+      // Form is invalid, show warnings
+      console.log('Form Value:', this.productForm.value);
+      console.log('Images Control Value:', this.productForm.get('images')?.value);
+    }
+  }
+
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  canvasRotation = 0;
+  rotation?: number;
+  translateH = 0;
+  translateV = 0;
+  scale = 1;
+  aspectRatio = 1 / 1;
+  showCropper = false;
+  containWithinAspectRatio = false;
+  transform: ImageTransform = {
+    translateUnit: 'px',
+  };
+  imageURL?: string;
+  loading = false;
+  allowMoveImage = false;
+  hidden = false;
+
+  cancel() {
+    this.showCropper = false;
+  }
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = '';
+    this.loading = true;
+    this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.objectUrl || event.base64 || '';
+    console.log(this.croppedImage);
+  }
+
+  imageLoaded() {
+    this.showCropper = true;
+    console.log('Image loaded');
+  }
+
+  cropperReady(sourceImageDimensions: Dimensions) {
+    console.log('Cropper ready', sourceImageDimensions);
+    this.loading = false;
+  }
+
+  loadImageFailed() {
+    console.error('Load image failed');
+  }
+
+  rotateLeft() {
+    this.loading = true;
+    setTimeout(() => {
+      // Use timeout because rotating image is a heavy operation and will block the ui thread
+      this.canvasRotation--;
+      this.flipAfterRotate();
+    });
+  }
+
+  rotateRight() {
+    this.loading = true;
+    setTimeout(() => {
+      this.canvasRotation++;
+      this.flipAfterRotate();
+    });
+  }
+
+  moveLeft() {
+    this.transform = {
+      ...this.transform,
+      translateH: ++this.translateH,
+    };
+  }
+
+  moveRight() {
+    this.transform = {
+      ...this.transform,
+      translateH: --this.translateH,
+    };
+  }
+
+  moveTop() {
+    this.transform = {
+      ...this.transform,
+      translateV: ++this.translateV,
+    };
+  }
+
+  moveBottom() {
+    this.transform = {
+      ...this.transform,
+      translateV: --this.translateV,
+    };
+  }
+
+  private flipAfterRotate() {
+    const flippedH = this.transform.flipH;
+    const flippedV = this.transform.flipV;
+    this.transform = {
+      ...this.transform,
+      flipH: flippedV,
+      flipV: flippedH,
+    };
+    this.translateH = 0;
+    this.translateV = 0;
+  }
+
+  flipHorizontal() {
+    this.transform = {
+      ...this.transform,
+      flipH: !this.transform.flipH,
+    };
+  }
+
+  flipVertical() {
+    this.transform = {
+      ...this.transform,
+      flipV: !this.transform.flipV,
+    };
+  }
+
+  resetImage() {
+    this.scale = 1;
+    this.rotation = 0;
+    this.canvasRotation = 0;
+    this.transform = {
+      translateUnit: 'px',
+    };
+  }
+
+  zoomOut() {
+    this.scale -= 0.1;
+    this.transform = {
+      ...this.transform,
+      scale: this.scale,
+    };
+  }
+
+  zoomIn() {
+    this.scale += 0.1;
+    this.transform = {
+      ...this.transform,
+      scale: this.scale,
+    };
+  }
+
+  toggleContainWithinAspectRatio() {
+    this.containWithinAspectRatio = !this.containWithinAspectRatio;
+  }
+
+  updateRotation() {
+    this.transform = {
+      ...this.transform,
+      rotate: this.rotation,
+    };
+  }
+
+  toggleAspectRatio() {
+    if (this.aspectRatio === 1 / 1) {
+      this.aspectRatio = 16 / 9;
+    } else if (this.aspectRatio === 16 / 9) {
+      this.aspectRatio = 9 / 16;
+    } else {
+      this.aspectRatio = 1 / 1;
+    }
+  }
+
+  getImagesAriaLabel(): string {
+    const imagesCount = this.product.images.length;
+    if (imagesCount === 0) {
+      return 'No images selected. Click to add images.';
+    } else {
+      return `${imagesCount} image${imagesCount > 1 ? 's' : ''} selected. Click to change.`;
+    }
+  }
+
+  onFileChange(event: any) {
+    this.imageChangedEvent = event;
+    this.showCropper = true;
+    this.getImagesAriaLabel();
   }
 
   formatPrice(item: any) {
@@ -95,28 +342,21 @@ export class AdminDashboardPageComponent {
   }
 
   toggleAvailable(item: any) {
+    this.productForm.markAllAsTouched();
+
+    const sizes = this.productForm.get('sizes');
+    if (sizes) {
+      sizes.updateValueAndValidity();
+      // replace the item in product.sizes with item
+      const index = this.product.sizes.findIndex((size) => size.size === item.size);
+      if (index >= 0) {
+        this.product.sizes[index] = item;
+      }
+      // update product form values
+    }
     item.available = !item.available;
     if (!item.available) {
-      //   item.price = '0.00'; // Reset price if the item is not available
     }
-  }
-
-  onSubmit() {
-    // Handle form submission
-    console.log('Product Data:', this.product);
-    // Here, you would add your logic to save data to Supabase
-  }
-
-  onFileChange(event:any) {
-    if (event.target.files && event.target.files.length) {
-      const files = event.target.files;
-      for (const file of files) {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.product.images.push(e.target.result);
-        };
-        reader.readAsDataURL(file);
-      }
-    }
+    this.productForm.get('sizes')?.setValue(this.product.sizes);
   }
 }

9bda68d4b68b54cec517ad3f0314e04a89b87d3d
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-12-31 14:36:18 +0000

    Refactor product description component and add edit functionality

--- src/app/features/product-page/component/description/description.component.ts
+++ src/app/features/product-page/component/description/description.component.ts
@@ -2,15 +2,31 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Subscription, switchMap} from 'rxjs';
 import {SupabaseService} from 'src/app/core/services/supabase.service';
-
+import {Subject} from 'rxjs';
 @Component({
   selector: 'app-description',
   template: `
     <!-- Product Description -->
-    <div class="bg-[#F7F7F7] p-6 rounded-lg shadow-sm w-full">
-      <h2 class="text-xl font-semibold text-gray-800 mb-4">Product Description</h2>
-      <p class="text-gray-600 mb-4">
-        {{ description | json }}
+    <!-- Product Description -->
+    <div class=" p-6 rounded-lg shadow-sm w-full dark:bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 border-black">
+      <div class="flex justify-between items-center">
+        <h2 class="text-xl font-semibold  mb-4">Apraksts</h2>
+        <button
+          (click)="editDescription()"
+          class="px-4 py-2 text-sm font-medium text-white bg-gray-500 rounded hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          {{ editBtnLabel }}
+        </button>
+      </div>
+      <textarea
+        *ngIf="isEditing"
+        class=" mb-4 border border-gray-300 rounded p-2 w-full dark:bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 border-black"
+        [(ngModel)]="description"
+        appAutoResize
+        rows="15"
+      ></textarea>
+      <p *ngIf="!isEditing" class=" mb-4 border border-gray-300 rounded p-2 overflow-auto">
+        {{ description || ' ' }}
       </p>
     </div>
   `,
@@ -20,6 +36,11 @@ export class DescriptionComponent implements OnInit {
   id: any = 0;
   description = '';
   routeSubscription: Subscription | undefined;
+  descriptionToEdit = '';
+  descriptionChange = new Subject<string>();
+
+  isEditing = false;
+  editBtnLabel: string = 'Edit' || 'Save';
 
   constructor(
     private db: SupabaseService,
@@ -52,5 +73,48 @@ export class DescriptionComponent implements OnInit {
       return '';
     }
     return data[0].description;
+  }
+
+  async editDescription() {
+    if (this.isEditing) {
+      // Save logic
+      console.log('Saving:', this.description);
+      await this.saveDescription(this.description);
+      this.isEditing = false;
+      this.editBtnLabel = 'Edit';
+    } else {
+      // Switch to edit mode
+      this.descriptionToEdit = this.description;
+      this.isEditing = true;
+      this.editBtnLabel = 'Save';
+    }
+  }
+
+  grabInnerText(event: any): void {
+    this.description = (event.target as HTMLElement).innerText;
+    console.log(this.description);
+  }
+  handleClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (this.isEditing && target.innerText.trim() === '') {
+      target.innerText = ' ';
+      const range = document.createRange();
+      const sel = window.getSelection();
+      range.setStart(target, 0);
+      range.collapse(true);
+      sel?.removeAllRanges();
+      sel?.addRange(range);
+    }
+  }
+
+  async saveDescription(text: string) {
+    console.log('Saving:', text);
+    const {data, error} = await this.db.supabase.from('products').update({description: text}).eq('product_id', this.id);
+    if (error) {
+      console.error(error);
+      return '';
+    }
+    console.log(data, 'data saved?');
+    return data;
   }
 }

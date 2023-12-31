6b7a675fcd7a2a9c7d485ccb80107ee2e5e5448c
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-12-31 10:03:53 +0000

    Update CSS styles for product card and home page component

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -4,9 +4,10 @@ import {Product} from 'src/app/core/services/product.service';
 @Component({
   selector: 'app-product-card',
   template: `
-    <div class="cursor-pointer rounded overflow-hidden shadow-lg" (click)="onClick()">
-      <app-product-image [imagePath]="product.images[0]"></app-product-image>
-      <div class="px-6 py-4 ">
+    <div class="cursor-pointer rounded overflow-hidden shadow-lg relative" (click)="onClick()">
+      <div class="absolute inset-0  transition-opacity duration-300 opacity-0 group-hover:opacity-100"></div>
+      <app-product-image class="bg-gradient" [imagePath]="product.images[0]"></app-product-image>
+      <div class="px-6 py-4 z-10 relative">
         <app-product-name [name]="product.name"></app-product-name>
         <app-product-price [price]="product.sizes[0].price" [currency]="product.currency"></app-product-price>
       </div>
@@ -18,15 +19,32 @@ import {Product} from 'src/app/core/services/product.service';
         text-align: center;
         display: grid;
         font-family: 'Arial', sans-serif;
-
         border-radius: 5px;
-        transition: all 0.3s ease-in-out;
+        position: relative;
+        overflow: hidden;
       }
 
-      :host:hover {
-        transform: scale(1.05);
-        background: radial-gradient(circle at center, #d3d3d37a, transparent);
-        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+      .bg-gradient {
+        background: linear-gradient(to bottom, rgba(10, 14, 20, 1) 10%, rgba(0, 255, 255, 0.1) 50%, rgba(10, 14, 20, 1) 90%);
+        background-size: 1% 200%;
+        animation: shine 3s linear infinite alternate;
+        backdrop-filter: blur(2px);
+      }
+
+      :host:hover .bg-gradient {
+        opacity: 1;
+        background: linear-gradient(to bottom, rgba(10, 14, 20, 1) 10%, rgba(0, 255, 255, 0.75) 50%, rgba(10, 14, 20, 1) 90%);
+      }
+
+      :host:not(:hover) .bg-gradient {
+        opacity: 1;
+        transition: opacity 0.5s ease-out;
+      }
+
+      @keyframes shine {
+        to {
+          background-position: 200% center;
+        }
       }
     `,
   ],

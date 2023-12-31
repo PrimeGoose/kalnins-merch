ddd38b9f2fb69589a784e7116e67f8baea96a8be
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2023-12-31 17:05:40 +0000

    Refactor my-orders component to display user profile and orders

--- src/app/features/my-orders-page/my-orders.component.ts
+++ src/app/features/my-orders-page/my-orders.component.ts
@@ -1,6 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
-
+import {SupabaseService} from '../../core/services/supabase.service';
+import {BehaviorSubject} from 'rxjs';
 @Component({
   selector: 'app-my-orders',
   standalone: true,
@@ -10,6 +11,19 @@ import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/co
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyOrdersComponent implements OnInit, OnDestroy {
-  ngOnInit() {}
+  orders: any = [];
+  private _user = new BehaviorSubject<any>({});
+  user$ = this._user.asObservable();
+  constructor(private db: SupabaseService) {}
+
+  async ngOnInit() {
+    await this.db.supabase.auth.getUser().then((data: any) => {
+      this._user.next(data.data.user.user_metadata);
+    });
+
+    //
+    const {data, error} = await this.db.supabase.from('orders').select('*');
+  }
+
   ngOnDestroy() {}
 }

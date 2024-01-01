99f5030869b096b4a3a47d1a49bb8c5e3ff5accc
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2024-01-01 14:50:55 +0000

    Refactor product-card.component.ts styles

--- src/app/shared/components/product-card/product-card.component.ts
+++ src/app/shared/components/product-card/product-card.component.ts
@@ -6,48 +6,14 @@ import {Product} from 'src/app/core/services/product.service';
   template: `
     <div class="cursor-pointer rounded overflow-hidden shadow-lg relative bg-gradient" (click)="onClick()">
       <div class="absolute inset-0  transition-opacity duration-300 opacity-0 group-hover:opacity-100"></div>
-      <app-product-image class="bg-gradient" [imagePath]="product.images[0]"></app-product-image>
+      <app-product-image class="brightness-animation" [imagePath]="product.images[0]"></app-product-image>
       <div class="px-6 py-4 z-10 relative">
         <app-product-name [name]="product.name"></app-product-name>
         <app-product-price [price]="product.sizes[0].price" [currency]="product.currency"></app-product-price>
       </div>
     </div>
   `,
-  styles: [
-    `
-      :host {
-        text-align: center;
-        display: grid;
-        font-family: 'Arial', sans-serif;
-        border-radius: 5px;
-        position: relative;
-        overflow: hidden;
-      }
-
-      .bg-gradient {
-        background: linear-gradient(to bottom, rgba(10, 14, 20, 1) 10%, rgba(0, 255, 255, 0.1) 50%, rgba(10, 14, 20, 1) 90%);
-        background-size: 1% 200%;
-        animation: shine 3s linear infinite alternate;
-        backdrop-filter: blur(2px);
-      }
-
-      :host:hover .bg-gradient {
-        opacity: 1;
-        background: linear-gradient(to bottom, rgba(10, 14, 20, 0.1) 10%, rgba(0, 255, 255, 0.2) 50%, rgba(10, 14, 20, 0.1) 90%);
-      }
-
-      :host:not(:hover) .bg-gradient {
-        opacity: 1;
-        transition: opacity 0.5s ease-out;
-      }
-
-      @keyframes shine {
-        to {
-          background-position: 200% center;
-        }
-      }
-    `,
-  ],
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent {
   @Input() product!: Product;

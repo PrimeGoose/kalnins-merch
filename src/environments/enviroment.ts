0904bd606be124aa69ee4c0d00be7326bfb5a881
PrimeGoose <64712362+PrimeGoose@users.noreply.github.com>
2024-03-09 16:10:59 +0000

    update db anon key

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,16 +1,9 @@
 export const environment = {
   production: false,
-  // ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzAwODcwNDAwLAogICJleHAiOiAxODU4NzIzMjAwCn0.27nXMRUosm-XVTZ--GhgKrHpUd3vJY4XkOyFUBG5_Zw
+
 
   supabaseUrl: 'https://islbmwzkwwjkjvbsalcp.reysweek.com',
   supabaseAnonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzAwODcwNDAwLAogICJleHAiOiAxODU4NzIzMjAwCn0.27nXMRUosm-XVTZ--GhgKrHpUd3vJY4XkOyFUBG5_Zw',
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzA5OTQyNDAwLAogICJleHAiOiAxODY3NzA4ODAwCn0.UCB76X5NnDIpb1oo0cLEsfxNKCEXCs2XNw8e2Ep8BJg"
 };
 
-// export const environment = {
-//   production: true,
-
-//   supabaseUrl: 'https://yghepighuybzgyjlqxhz.supabase.co',
-//   supabaseAnonKey:
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnaGVwaWdodXliemd5amxxeGh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY3MzkzNjEsImV4cCI6MjAxMjMxNTM2MX0.RbPcVB1Hj_iYCWYzYU3mmWWaHrmgLx7FxPEfpL-oniE',
-// };
